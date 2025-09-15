8d07b0151dacc9de0c8e5ed2ad020de0271f5c1e
Preston Jackson <preston.jackson@gmail.com>
2025-09-15 08:40:33 -0600

    Presenting a default blog and research page when no specific blog is requested. Also, stripping the .md off of the filename in the URL (as it is an implementation detail.

--- bin/convert-markdown.py
+++ bin/convert-markdown.py
@@ -1,21 +1,33 @@
 #! /usr/bin/python3
 
+import glob
 import os
 import subprocess
+import urllib.parse
 
 # Print required CGI headers
 print("Content-type: text/html")
 print("", flush=True)  # End of headers
 
+args = urllib.parse.parse_qs(os.environ['QUERY_STRING'])
+dir = args["dir"][0]
+
 root = os.environ['DOCUMENT_ROOT']
-request = os.environ['REQUEST_URI']
+filename = os.environ['REQUEST_URI']
+
+filepath = f"{root}{filename}.md"
+
+# If the file doesn't exist, use the latest Markdown file in the directory
+if not os.path.isfile(filepath):
+    filepath = sorted(glob.glob(f'{root}/{dir}/*.md'), reverse=True)[0]
 
 subprocess.run(['cmark-gfm',
-                '-t', 'html',
-                '--smart',
+                '-t', 'html',  # Output format is HTML
+                '--smart',  # Enable smart punctuation
                 '--validate-utf8',
                 '-e', 'footnotes',
                 '-e', 'table',
                 '-e', 'strikethrough',
                 '-e', 'tasklist',
-                f"{root}{request}"], check=True)
+                filepath],
+                check=True)
--- bin/get-recents.py
+++ bin/get-recents.py
@@ -6,16 +6,18 @@ import urllib.parse
 
 # Print required CGI headers
 print("Content-type: text/html")
-print("")  # End of headers
+print("", flush=True)  # End of headers
 
 args = urllib.parse.parse_qs(os.environ['QUERY_STRING'])
 dir = args["dir"][0]
-doc_root = os.environ['DOCUMENT_ROOT']
 
-for filename in sorted(os.listdir(f'{doc_root}/{dir}'), reverse=True):
+root = os.environ['DOCUMENT_ROOT']
+
+for filename in sorted(os.listdir(f'{root}/{dir}'), reverse=True):
     if filename.endswith('.md'):
         match = re.search(r'^(.+)_(.+)\.md$', filename)
         if match:
             date_part = match.group(1)
+            basename = os.path.splitext(filename)[0]
             title_part = match.group(2).replace('-', ' ').title()
-            print(f"<li><a href=\"/{dir}/{filename}\">{date_part} {title_part}</a></li>")
+            print(f"<li>{date_part} <a href=\"/{dir}/{basename}\">{title_part}</a></li>")
